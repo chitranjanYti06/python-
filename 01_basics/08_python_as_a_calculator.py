957ff0d14a471aec9ee4fa4c739d8db0ee0df34c
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-11 04:35:20 +0000

    precedence done

---
+++ 01_basics/08_python_as_a_calculator.py
@@ -0,0 +1,39 @@
+# python as a calculator
+
+print(2+3*4)
+
+#
+# +   addition
+# -    sutraction
+
+# *   multiplication
+# /   float division
+
+# //  integer division
+
+# % modulo
+# ** exponent
+
+# print(2/4)
+# print(2//4)
+
+# print(4//2)
+
+# print(2**2)
+
+# print(2**0.5)
+# print(round(  2**0.5))
+
+print(round(2**0.5,4))
+
+print(2**3/2*6-4*(3-4/2))
+
+
+#precedence Rule
+
+# operator                        precedence and associative Rule
+
+# paranthesis                     highest
+# exponent                        right to left
+# *, /,//,%                       left to right
+# +,-                             left to right
