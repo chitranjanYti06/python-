f1b40f58f20e48adf8c46d3d19fbb9d574ee6db1
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-12 06:07:11 +0000

    summary one chaoter one

---
+++ 01_basics/10_Summary.py
@@ -0,0 +1,24 @@
+#print Function
+print("chtranjan ' abc' ")
+
+#escape sequesce
+print("line A \t lineB")
+
+#escape sequence as normal text
+print(" this is backslash \\")
+print(r" this is line a \n lineb")
+
+#float division(4/2)
+#integer division (4//2)
+
+# print(2**3)
+
+#variable
+# python is dynamic progrmming so don't need to give the data type of this programming language it is alreary decide on the run time
+# name1 = "chitranjan"
+# name1=123
+
+# convension 
+
+# user_one =123 snake case convention
+# userOneName ="abc" camel case convention
