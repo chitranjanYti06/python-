404d7538bec9738649bcb109a065bd88d7d70cad
chitranjanYti06 <142107399+chitranjanYti06@users.noreply.github.com>
2024-01-12 05:57:01 +0000

    variables

---
+++ 01_basics/09_variables.py
@@ -0,0 +1,26 @@
+#variables
+
+# number1 = 2
+# print(number1)
+# number2 =4
+# print(number2)
+# name="chitranjan"
+# print(name)
+# name=123
+# print(name
+    #   )
+
+#python ek dynamic progrming langusge hai kyoki runtime pe decide hota hai ki iska type kya hoga
+
+# variable naming rule
+
+#1number = 4 wrong
+# _name = "chitranjan" can start with letter and underscore
+# cannot use special symbol
+
+# convention for variable
+
+user_One_name = "chitranjan" #snake case writing
+
+userOneName = "chittu"  #camel case writing
+
